0264800406f954ea5718654c81d6844edb8e0c8a
BunsDND <itsmevance19@gmail.com>
2025-01-30 15:57:28 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './pages/Home'
+import Navbar from './compontents/Navbar'
+
+
+export default function App() {
+  return (
+    <>
+   
+    <div><Navbar/>
+      <Home/> 
+    </div>
+    </>
+    
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,49 @@
+import  { useState } from 'react';
+import './Home.css'; // Import the external CSS file
+
+const randomFacts = [
+  "Honey never spoils.",
+  "Bananas are berries, but strawberries aren't.",
+  "A group of flamingos is called a 'flamboyance'.",
+  "Octopuses have three hearts.",
+  "Wombat poop is cube-shaped.",
+];
+
+function getRandomFact() {
+  const randomIndex = Math.floor(Math.random() * randomFacts.length);
+  return randomFacts[randomIndex];
+}
+
+export default function Home() {
+  const [fact, setFact] = useState(getRandomFact());
+
+  const handleNewFact = () => {
+    setFact(getRandomFact());
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">Hello World</h1>
+      <p className="content">
+        Welcome to my random facts page! Here you will find interesting and fun facts that you might not know.
+      </p>
+      <div className="random-fact">
+        <strong>Random Fact:</strong> {fact}
+      </div>
+      <button className="new-fact-button" onClick={handleNewFact}>
+        Get Another Random Fact
+      </button>
+
+      <h2 className="subtitle">More Interesting Facts</h2>
+      <ul className="fact-list">
+        {randomFacts.map((fact, index) => (
+          <li key={index} className="fact-item">{fact}</li>
+        ))}
+      </ul>
+
+      <div className="image-container">
+        <img src="https://media.istockphoto.com/id/1399292810/photo/group-of-wildlife-animals-in-the-jungle-together.jpg?s=612x612&w=0&k=20&c=NXVzp7awiZhUf-OjcSmaDTcWz3h_XyGcozlTFD883eg=" alt="Random" className="random-image" />
+      </div>
+    </div>
+  );
+}
